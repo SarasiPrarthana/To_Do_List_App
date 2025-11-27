3a7693a755b6ca02c3d078691148a5d2d367bbca
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-11-27 10:58:44 +0530

    Add Controller

--- src/main/java/controller/DashBoardController.java
+++ src/main/java/controller/DashBoardController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class DashBoardController {
+
+    @FXML
+    private DatePicker txtDate;
+
+    @FXML
+    private TextField txtDescription;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtNew;
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
 }
---
+++ src/main/java/model/dto/CompletedDashBoardDTO.java
@@ -0,0 +1,12 @@
+package model.dto;
+
+import java.util.Date;
+
+public class CompletedDashBoardDTO {
+
+    private String completedId;
+    private String id;
+    private String title;
+    private String description;
+    private Date Donedate;
+}
