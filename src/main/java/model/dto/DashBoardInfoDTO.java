37e196cdf767cfb08aa7f2cb7c8750efa2315630
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-11-27 10:36:58 +0530

    Create Attributes

--- src/main/java/model/dto/DashBoardInfoDTO.java
+++ src/main/java/model/dto/DashBoardInfoDTO.java
@@ -1,4 +1,11 @@
 package model.dto;
 
+import java.util.Date;
+
 public class DashBoardInfoDTO {
+
+    private String id;
+    private String title;
+    private String description;
+    private Date date;
 }
