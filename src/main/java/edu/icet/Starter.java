4ede7a3d4ea4072f2be66c7b370a8f49ef826efe
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-11-27 10:12:04 +0530

    Create Main and Starter

---
+++ src/main/java/edu/icet/Starter.java
@@ -0,0 +1,12 @@
+package edu.icet;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args){ launch(args);}
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
